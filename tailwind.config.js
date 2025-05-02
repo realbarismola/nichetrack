f76ffcda9e3b350e31c64763857fd00af7c2ac8f
Baris <baris.mola@gmail.com>
2025-05-02 11:58:49 +0200

    Deploy latest API update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
     theme: {
       extend: {},
     },
-    plugins: [],
+    plugins: [
+      require('@tailwindcss/forms'),
+      require('@tailwindcss/typography'),
+      require('@tailwindcss/aspect-ratio'),
+    ],
   };
   

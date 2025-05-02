41042fd2cca3dded5bba95dc2e72efe8b3b95a9e
Baris <baris.mola@gmail.com>
2025-05-02 10:58:57 +0200

    Deploy latest API update

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+export const dynamic = 'force-dynamic';
+
+
 import { supabase } from '@/lib/supabaseClient';
 import { Auth } from '@supabase/auth-ui-react';
 import { ThemeSupa } from '@supabase/auth-ui-shared';

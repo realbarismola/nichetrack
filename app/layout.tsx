27da23386f9c954bd24bc5daa7c1d78dfff401a4
realbarismola <131979876+realbarismola@users.noreply.github.com>
2025-06-02 11:02:17 +0200

    Updated layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en\" suppressHydrationWarning>
       <body className={`${roboto.variable} ${robotoMono.variable} antialiased`}>
         <ThemeProvider
           attribute="class"
